64098c3c14f6090362f5f7418816a0b4b022aa00
Loris <loris.vianellos@gmail.com>
2017-06-15 09:13:26 +0200

    #0

--- prova/tool.loader.js
+++ prova/tool.loader.js
@@ -2,7 +2,8 @@ var toolloader = {
     run: function() {
         console.log('Loading...');
 		for (var ksp in Game.spawns) {
-				var sou=Game.spawns[ksp].room.find('FIND_SOURCES')
+				console.log(JSON.stringify(Game.spawns[ksp]))
+				var sou=Game.spawns[ksp].room.find('FIND_SOURCES_ACTIVE')
 				console.log(JSON.stringify(sou))
 		}
 
