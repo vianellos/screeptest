82ace4e4e93cb27bdca134a8452f22273641afa1
Loris <loris.vianellos@gmail.com>
2017-06-15 09:53:09 +0200

    #0

--- prova/tool.loader.js
+++ prova/tool.loader.js
@@ -2,7 +2,7 @@ var toolloader = {
     run: function() {
         console.log('Loading...');
 		for (var ksp in Game.spawns) {
-				sou=Game.spawns[ksp].room.find(FIND_SOURCES)
+				var sou=Game.spawns[ksp].room.find(FIND_SOURCES)
 				for (var kso in sou) {
 					console.log(sou[kso]);
 				}
