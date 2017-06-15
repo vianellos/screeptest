086a0249d7ec0e17ccf55300234d9b7a5fefb48f
Loris <loris.vianellos@gmail.com>
2017-06-15 09:51:44 +0200

    #0

--- prova/tool.loader.js
+++ prova/tool.loader.js
@@ -2,17 +2,11 @@ var toolloader = {
     run: function() {
         console.log('Loading...');
 		for (var ksp in Game.spawns) {
-				console.log(JSON.stringify(Game.spawns[ksp]))
-				var sou=Game.spawns[ksp].room.find('FIND_SOURCES')
-				console.log(JSON.stringify(sou))
+				sou=Game.spawns[ksp].room.find(FIND_SOURCES)
+				for (var kso in sou) {
+					console.log(sou[kso]);
+				}
 		}
-
-		for (var kro in Game.rooms) {
-				console.log(JSON.stringify(Game.rooms[kro]))
-				var sou=Game.rooms[kro].find('FIND_SOURCES')
-				console.log(JSON.stringify(sou))
-		}
-
     }
 
 }
